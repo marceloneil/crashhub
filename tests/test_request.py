3641e47a057d0b173d24128505629761c650dacf
Marcel O'Neil <marcel@marceloneil.com>
2018-03-06 14:28:22 -0500

    test

--- tests/test_request.py
+++ tests/test_request.py
@@ -45,8 +45,8 @@ def test_create(crashhub_client):
 def test_updated(crashhub_client):
     for _ in range(2):
         response = crashhub_client.post("/crash", data=request2)
-    github.Github.return_value.get_repo.return_value.get_issue.assert_called_once()
     github.Github.return_value.get_repo.return_value.create_issue.assert_called_once()
+    github.Github.return_value.get_repo.return_value.get_issue.assert_called_once()
 
 def test_rate_limit(crashhub_client):
     for _ in range(5):
